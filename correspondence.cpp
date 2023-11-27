2fc0af3a47252bea32038b6fc87f3c1c5b67ab0b
G0oCe <alim.sadigov20@gmail.com>
2023-11-28 01:37:36 +0400

    implement the function

--- correspondence.cpp
+++ correspondence.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <unordered_map>
+
+bool canConstruct(std::string ransomLetter, std::string paper);
 
 int main() {
+	std::cout << canConstruct("aa", "ab") << std::endl; 
+    std::cout << canConstruct("aa", "aab") << std::endl;
 
 	return 0;
+}
+
+bool canConstruct(std::string ransomLetter, std::string paper) {
+    std::unordered_map<char, int> charCount;
+
+    for (char a : paper) {
+        charCount[a]++;
+    }
+
+    for (char c : ransomLetter) {
+        if (charCount.find(c) == charCount.end() || charCount[c] == 0) {
+            return false; 
+        } else {
+            charCount[c]--;
+        }
+    }
+
+    return true;
 }
