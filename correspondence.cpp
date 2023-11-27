9a5931ec71d8970eab758d39c21df9fa63603d5c
G0oCe <alim.sadigov20@gmail.com>
2023-11-28 02:13:16 +0400

    Add comments

--- correspondence.cpp
+++ correspondence.cpp
@@ -5,7 +5,7 @@
 bool canConstruct(std::string ransomLetter, std::string paper);
 
 int main() {
-    std::cout << std::boolalpha;
+    std::cout << std::boolalpha; //Add stream manipulator
 
 	std::string paper;
 	std::string ransomLetter;
@@ -19,15 +19,17 @@ int main() {
 }
 
 bool canConstruct(std::string ransomLetter, std::string paper) {
+    // Creating an unordered_map to count the number of each letter in the paper
     std::unordered_map<char, int> charCount;
 
     for (char a : paper) {
         charCount[a]++;
     }
 
+    // Check if there are enough letters in unordered_map to compose a ransomLetter
     for (char c : ransomLetter) {
         if (charCount.find(c) == charCount.end() || charCount[c] == 0) {
-            return false;
+            return false; // The letter is missing or there are not enough letters
         } else {
             charCount[c]--;
         }
